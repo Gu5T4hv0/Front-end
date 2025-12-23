c1d88a67b049e765f12594dc926dd095d2e78af2
Gu5T4hv0 <vogoostah@gmail.com>
2025-12-23 12:14:59 -0300

    a bunch of stuff

---
+++ Rocketseat/Projeto/script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("lighty")
+    
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // substituir a imagem
+    if(html.classList.contains("lighty")){
+    // se tiver light mode, add imagem light
+    img.setAttribute("src", './assetos/avatar-light.png')
+    }
+    else{
+        img.setAttribute("src", './assetos/avatar.png')
+    }
+
+
+    
+    // se tiver sem light mode, manter a imagem normal
+}
