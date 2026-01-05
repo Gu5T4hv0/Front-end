3057e34ec56ffaaef60f86ec9e5c210a7acbf0c1
Gu5T4hv0 <vogoostah@gmail.com>
2026-01-05 18:55:10 -0300

    last adjustments

---
+++ The Odin Project/JS/script.js
@@ -0,0 +1,40 @@
+console.log("Hello, World!")
+
+let firstName = "Arthur";
+let lastName = "Souza";
+
+console.log(firstName + " " + lastName)
+
+let age = 55;
+console.log(age)
+
+age = 67;
+console.log(age)
+
+const pi = 3.14;
+console.log(pi)
+
+// let calc = (3 + 2) - 76 * (1 + 1);
+// console.log(calc)
+
+console.log((3 + 2) - 76 * (1 + 1))
+
+let a = 5;
+let b = 3;
+let c = 9;
+console.log(a*c/b)
+
+const max = 57;
+const actual = max - 13;
+const percentage = (actual/max) / 0.1;
+console.log(percentage)
+
+let p = 300;
+let k = 4;
+let bob = 5.3;
+console.log(p+k/bob)
+
+let declaração = "eu";
+let de = "te";
+let amor = "amo";
+console.log(declaração + " " + de + " " + amor)
