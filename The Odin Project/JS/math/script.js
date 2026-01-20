759c807bf2426697b3d6355f148f1f8f22e2fe83
Gu5T4hv0 <vogoostah@gmail.com>
2026-01-19 23:17:44 -0300

    JS

---
+++ The Odin Project/JS/math/script.js
@@ -0,0 +1,43 @@
+let num1 = 2;
+let num2 = 34;
+let num3 = 22;
+let num4 = 73;
+
+let number = num1 + num2;
+let number2 = num3 - num4;
+
+let finalResult = number * number2;
+
+evenOddResult = finalResult % 2;
+
+
+if (finalResult % 2 === 0) {
+    console.log("even")
+}
+
+let result = 7 + 13 / 9 + 7;
+let result2 = (100 / 2) * 6;
+
+result = result * result2
+console.log(result)
+
+let finalResult2 = result.toFixed(2)
+console.log(finalResult2)
+
+console.log(typeof finalResult2)
+finalNumber = Number(finalResult2)
+console.log(typeof finalNumber)
+
+// Statement 1: The elephant weighs less than the mouse
+const eleWeight = 1000;
+const mouseWeight = 2;
+// Statement 2: The Ostrich is taller than the duck
+const ostrichHeight = 2;
+const duckHeight = 0.3;
+// Statement 3: The two passwords match
+const pwd1 = "stromboli";
+const pwd2 = "stROmBoLi";
+
+weightComparison = eleWeight < mouseWeight;
+heightComparison = ostrichHeight > duckHeight;
+pwdMatch = pwd1 === pwd2;
