cf6a04b6e5850fe2cc708ad3a0dae3fafec4742c
Gu5T4hv0 <vogoostah@gmail.com>
2026-07-22 11:41:24 -0300

    volta pro assignments negão

--- The Odin Project/JS/loops/one.js
+++ The Odin Project/JS/loops/one.js
@@ -29,3 +29,60 @@ for (let i = 0; i < cats.length; i++) {
 }
 
     console.log(myFavoriteCats);
+
+// ------------------------------------
+function toUpper(string) {
+    return string.toUpperCase();
+}
+
+const cats = ['leo', 'jagua', 'curaat', 'dudinhaGatinha'];
+
+const mapper = cats.map(toUpper);
+
+console.log(mapper);
+
+function lcat(string) {
+    return string.startsWith('l');
+}
+
+const cats = ['leo', 'jagua', 'curaat'];
+
+const filterCats = cats.filter(lcat);
+
+console.log(filterCats);
+
+// ---------------------------
+
+const cats = ['leo', 'jagua', 'curaat', 'dudinhaGatinha'];
+
+for (let i = 0; i < cats.length; i++){
+    console.log(cats[i]);
+}
+
+// =================================
+
+const prices = [10, 34, 21, 65, 9];
+
+const total = prices.reduce((accumulator, element) => accumulator + element);
+
+console.log(total);
+
+// -------
+
+const array = [1,2,3,4,5,6,7,8,9,10];
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function sumOfTripledEvens() {
+    const takeEvens = array.filter(isEven);
+    const tripleEvens = takeEvens.map((num) => num * 3);
+    const totalSum = tripleEvens.reduce((acc, curr) => acc + curr);
+    return totalSum;
+}
+console.log(sumOfTripledEvens(array));
+
+const takeEvens = array.filter(isEven);
+const tripleEvens = takeEvens.map((num) => num * 3);
+const totalSum = tripleEvens.reduce((acc, curr) => acc + curr, 0);
+console.log(totalSum);
